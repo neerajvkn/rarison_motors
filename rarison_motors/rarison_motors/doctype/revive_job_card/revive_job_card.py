4599b98a357ef8d93da553665210c48466b1567f
Frappe <frappe@vmi3484951.contaboserver.net>
2026-09-08 19:18:18 +0000

    feat: added cancelled job card modify validation

--- rarison_motors/rarison_motors/doctype/revive_job_card/revive_job_card.py
+++ rarison_motors/rarison_motors/doctype/revive_job_card/revive_job_card.py
@@ -6,4 +6,11 @@ from frappe.model.document import Document
 
 
 class ReviveJobCard(Document):
-	pass
+	def validate(self):
+		if not self.is_new():
+			previous_status = frappe.db.get_value(
+				"Revive Job Card", self.name, "status"
+			)
+
+			if previous_status == "Cancelled":
+				frappe.throw(_("Cancelled Job Cards cannot be modified."))
